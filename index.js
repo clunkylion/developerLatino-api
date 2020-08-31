52db787289ea74c725096ffecbebc105a9a4e968
José Cortés <cote99salamanca@gmail.com>
2020-08-30 23:06:58 -0400

    add body parser

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const { config } = require('./config/index');
 const { videosApi } = require('./routes/videos.js');
-
+//body parser middleware
+app.use(express.json());
 videosApi(app);
 app.listen(config.port, () => {
   console.log(`Listening http://localhost:${config.port}`);
