aebfeb42e10ab9f377a31a718251df06d0d8a83a
José Cortés <cote99salamanca@gmail.com>
2020-08-30 20:02:07 -0400

    add services folder and videoService

---
+++ services/videos.js
@@ -0,0 +1,27 @@
+const { videosMock } = require('../utils/mocks/videos');
+
+class VideosService {
+  async getVideos() {
+    const videos = await Promise.resolve(videosMock);
+    return videos || [];
+  }
+  async getVideo() {
+    const videos = await Promise.resolve(videosMock[0]);
+    return videos || {};
+  }
+  async createVideo() {
+    const createVideoId = await Promise.resolve(videosMock[0].id);
+    return createVideoId || {};
+  }
+  async updateVideo() {
+    const updateVideoId = await Promise.resolve(videosMock[0].id);
+    return updateVideoId;
+  }
+  async deleteVideo() {
+    const deleteVideoId = await Promise.resolve(videosMock[0].id);
+    return deleteVideoId;
+  }
+}
+module.exports = {
+  VideosService,
+};
