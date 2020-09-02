512e780eb7d01e873702fa2cc98f0f879945a5fa
José Cortés <cote99salamanca@gmail.com>
2020-09-01 23:50:06 -0400

    use new middlewares

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const { config } = require('./config/index');
 const { videosApi } = require('./routes/videos.js');
 
 //add error middleware
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandler');
+const {
+  logErrors,
+  errorHandler,
+  wrapErrror,
+} = require('./utils/middleware/errorHandler');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 //body parser middleware
 app.use(express.json());
+//routes
 videosApi(app);
+//catch 404 error
+app.use(notFoundHandler);
 //los middlewares de errores siempre deben ir al final de las rutas
 app.use(logErrors);
+app.use(wrapErrror);
 app.use(errorHandler);
 app.listen(config.port, () => {
   console.log(`Listening http://localhost:${config.port}`);
