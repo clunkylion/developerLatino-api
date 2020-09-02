ef6256d4e8bde5ba7ab94b8d218c5cd97c2c0af8
José Cortés <cote99salamanca@gmail.com>
2020-09-01 23:17:13 -0400

    app use error middleware

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const { config } = require('./config/index');
 const { videosApi } = require('./routes/videos.js');
+
+//add error middleware
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandler');
 //body parser middleware
 app.use(express.json());
 videosApi(app);
+//los middlewares de errores siempre deben ir al final de las rutas
+app.use(logErrors);
+app.use(errorHandler);
 app.listen(config.port, () => {
   console.log(`Listening http://localhost:${config.port}`);
 });
