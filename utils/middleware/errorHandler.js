ba77e9d556a7413f09b8e181f8555f306b0c8ea1
José Cortés <cote99salamanca@gmail.com>
2020-09-01 23:49:49 -0400

    add new middlewares

--- utils/middleware/errorHandler.js
+++ utils/middleware/errorHandler.js
@@ -1,8 +1,9 @@
+const boom = require('@hapi/boom');
 const { config } = require('../../config');
 
 function withErrorStack(error, stack) {
   if (config.dev) {
-    return { error, stack };
+    return { ...error, stack };
   }
   return error;
 }
@@ -11,13 +12,23 @@ function logErrors(err, req, res, next) {
   console.log(err);
   next(err);
 }
+function wrapErrror(err, req, res, next) {
+  if (!err.isBoom) {
+    next(boom.badImplementation(err));
+  }
+  next(err);
+}
 // eslint-disable-next-line no-unused-vars
 function errorHandler(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json(withErrorStack(err.message, err.stack));
+  const {
+    output: { statusCode, payload },
+  } = err;
+  res.status(statusCode);
+  res.json(withErrorStack(payload, err.stack));
 }
 
 module.exports = {
   logErrors,
+  wrapErrror,
   errorHandler,
 };
